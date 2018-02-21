912877939def7e79ef63bff6995a7b89443e3bed
Frank Hickman <fhickman@akiliinteractive.com>
2018-02-21 14:27:53 -0800

    Handle Android RN 0.47 breaking change

--- android/src/main/java/com/masteratul/rn/testexception/RnTestExceptionHandlerPackage.java
+++ android/src/main/java/com/masteratul/rn/testexception/RnTestExceptionHandlerPackage.java
@@ -16,7 +16,7 @@ public class RnTestExceptionHandlerPackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new RnTestExceptionHandlerModule(reactContext));
     }
 
-    @Override
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
