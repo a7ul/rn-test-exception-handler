bfc8ed907cdc2634fb0d33554ae51a0af807126d
TomMahle <tommahle@gmail.com>
2018-04-06 11:25:24 -0500

    Removes outdated override
    Per https://github.com/facebook/react-native/commit/ce6fb337a146e6f261f2afb564aa19363774a7a8

--- android/src/main/java/com/masteratul/rn/testexception/RnTestExceptionHandlerPackage.java
+++ android/src/main/java/com/masteratul/rn/testexception/RnTestExceptionHandlerPackage.java
@@ -15,12 +15,7 @@ public class RnTestExceptionHandlerPackage implements ReactPackage {
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
       return Arrays.<NativeModule>asList(new RnTestExceptionHandlerModule(reactContext));
     }
-
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
-    }
-
+    
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Collections.emptyList();
